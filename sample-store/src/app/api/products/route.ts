d45c8ae5dbd8f63a1a24337be77af675e57942ab
Dan-W1210 <giulietta33@o3i5o.com>
2026-06-03 13:51:19 +0900

    feat: 商品一覧および個別取得APIの追加

---
+++ sample-store/src/app/api/products/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const products = [
+  {id: 1, name: "アロマキャンドル", price: 1980 },
+  {id: 2, name: "ドライフラワー", price: 2580 },
+  {id: 3, name: "ガラス花瓶", price: 1200 },
+  {id: 4, name: "ルームスプレイ", price: 980}
+];
+
+//全商品データを取得
+export async function GET() {
+  return NextResponse.json(products);
+}
